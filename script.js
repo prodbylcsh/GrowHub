928e14addfc4e3ca7a078c3854fe2fad259bfa03
Lukáš Schöbel <107074516+prodbylcsh@users.noreply.github.com>
2026-07-23 00:38:28 +0200

    supabase project info added

---
+++ script.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://vnutrvpsvqoveihpgibm.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Kw-0YWYsqSdL07CMlgyoUg_RIrB4-Ff";
+
+const supabaseClient = supabase.createClient({
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY,
+});
