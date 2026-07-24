f8407adff55d2f7a71e45f629a857249454dde49
Lukáš Schöbel <107074516+prodbylcsh@users.noreply.github.com>
2026-07-24 18:15:30 +0200

    upraven link

--- registered.js
+++ registered.js
@@ -2,6 +2,7 @@ const discordInvite = sessionStorage.getItem("discordInvite");
 
 if (discordInvite) {
     const linkEl = document.querySelector("#discord-invite-link");
+    linkEl.href = discordInvite;
     linkEl.textContent = discordInvite;
 
     sessionStorage.removeItem("discordInvite");
